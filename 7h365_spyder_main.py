11a144e044774ee5960ba7f46c88a20c27742844
yao16 <381067993@qq.com>
2019-07-31 23:19:23 +0800

    修复 ValueError: All strings must be XML compatible: Unicode or ASCII, no NULL bytes or control characters

--- 7h365_spyder_main.py
+++ 7h365_spyder_main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import requests
 import re
 from bs4 import BeautifulSoup as bs
@@ -5,7 +6,8 @@ from bs4 import BeautifulSoup as bs
 from docx import Document
 from docx.shared import RGBColor
 from docx.shared import Pt
-
+from docx.oxml.ns import qn
+#http://www.7h365.com/plugin.php?id=votes:view&appid=4815中有特殊字符
 url = 'http://www.7h365.com/plugin.php?id=votes%3Alist&actid=180page%3D4&page='
 person_page_url = 'http://www.7h365.com/plugin.php?id=votes:view&appid='
 
@@ -64,29 +66,44 @@ def load_person_info(actid, vote_list_page_count):
                 print(person_info.text)
                 run = p.add_run(person_info.text + '\n')
                 run.font.size = Pt(16)
-               # run.font.name = u'宋体'
+                run.font.name = u'宋体'
+                run._element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体')
+                run.bold = True
+
             if len(person_page_soup.select('.jianjie')) > 0:
                 print('自荐综述')
-                run = p.add_run(u'自荐综述')
-                run.font.size = Pt(16)
+                run = p.add_run(u'自荐综述\n')
+                run.font.size = Pt(12)
+                run.font.name = u'微软雅黑'
+                run._element.rPr.rFonts.set(qn('w:eastAsia'), u'微软雅黑')
                 for item in person_page_soup.select('.jianjie'):
                     print(item.text)
-                    run = p.add_run(item.text)
-                    run.font.size = Pt(16)
+                    if item.text != None and len(item.text) > 0:
+                        run = p.add_run(item.text)
+                        run.font.size = Pt(10.5)
+                        run.font.name = u'微软雅黑'
+                        run._element.rPr.rFonts.set(qn('w:eastAsia'), u'微软雅黑')
             if len(person_page_soup.select('.art-title')) > 0:
                 print('参评报告')
-                run = p.add_run(u'参评报告')
-                run.font.size = Pt(16)
+                run = p.add_run(u'参评报告\n')
+                run.font.size = Pt(12)
+                run.font.name = u'微软雅黑'
+                run._element.rPr.rFonts.set(qn('w:eastAsia'), u'微软雅黑')
                 for item in person_page_soup.select('.art-title'):
                     print(item.text)
-                    run = p.add_run(item.text)
-                    run.font.size = Pt(16)
+                    if item.text != None and len(item.text) > 0:
+                        run = p.add_run(re.sub('', '', item.text))
+                        run.font.size = Pt(10.5)
+                        run.font.name = u'微软雅黑'
+                        run._element.rPr.rFonts.set(qn('w:eastAsia'), u'微软雅黑')
             print('**************************************************************')
             run = p.add_run(u'**************************************************************')
             run.font.size = Pt(16)
+            run.font.name = u'宋体'
+            run._element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体')
             total_count += 1
         vote_list_current_page += 1
-    document.save('c:/Users/yaoxiao/Desktop/test3.docx')
+    document.save('c:/Users/yao16/Desktop/test180.docx')
     print(total_count)
 
 #抓取“最佳宏观策略分析师”actid = 180
@@ -98,11 +115,11 @@ def load_person_info(actid, vote_list_page_count):
 #抓取“最佳工业品期货分析师”actid = 182
 #load_person_info(182, 19)
 
-#抓取“最佳农副产品期货分析师”actid = 183
-#load_person_info(183, 4)
+#抓取“最佳农副产品期货分析师”actid = 183 //有问题
+load_person_info(183, 4)
 
 #抓取“最佳金融量化策略工程师”actid = 184
-load_person_info(184, 1)
+#load_person_info(184, 1)
 
 #抓取“最佳期权分析师”actid = 185
 #load_person_info(185, 2)
